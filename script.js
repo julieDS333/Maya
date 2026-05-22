f8bc42749ae90ec7b9e7dd7685240006c2e28de4
julieDS333 <juliedesousa99@gmail.com>
2026-05-22 14:00:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,8 @@ var ASSETS = "";
 var GREETINGS = [
   "Dear Maya's friend, I am Carlos the Oracle.",
   "Dear Maya's lover, I am Carlos the Oracle.",
-  "Dear Maya's cousin, I am Carlos the Oracle.",
   "Dear Maya's colleague who wasn't sure about coming tonight, I am Carlos the Oracle.",
   "Dear Maya's person, I am Carlos the Oracle.",
-  "Dear mysterious guest, I am Carlos the Oracle.",
   "Dear Maya's favorite — don't tell the others — I am Carlos the Oracle.",
   "Dear whoever Maya dragged here tonight, I am Carlos the Oracle."
 ];
